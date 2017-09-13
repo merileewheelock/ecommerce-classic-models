888294bf15faf39df34ed82354c1c78013b66b08
Merilee Wheelock <eelirem@gmail.com>
2017-09-12 17:24:48 -0700

    added Contact page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './containers/Home';
 import NavBar from './containers/NavBar';
 import Slick from './components/Slick';
 import About from './components/About';
+import Contact from './components/Contact';
 import Register from './containers/Register';
 import Login from './containers/Login';
 import ProductLine from './containers/ProductLine';
@@ -22,6 +23,7 @@ class App extends Component {
 					<div className="container main">
 						<Route exact path="/" component={Home} />
 						<Route exact path="/about" component={About} />
+						<Route exact path="/contact" component={Contact} />
 						<Route exact path="/register" component={Register} />
 						<Route exact path="/login" component={Login} />
 						<Route path="/shop/:productLine" component={ProductLine}/>
---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default function(){
+	return(
+		<div>
+			<h1>CONTACT</h1>
+			<hr />
+			<p>Merilee Wheelock</p>
+			<p>merilee.wheelock@gmail.com</p>
+			<p><a href="http://www.merileewheelock.com" target="_blank">www.merileewheelock.com</a></p>
+		</div>
+	)
+}
